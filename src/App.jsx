8e2e199aceab4387c12e9aff1cccd4df98ee5fa6
Chris Snoek <chrissnoek@Chriss-MacBook-Pro.local>
2019-09-23 20:53:44 +0200

    Create first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-const cont = ["Africa", "America", "AsiaAA"];
-const helloCont = Array.from(cont, c => `Hello ${c}`);
-const message = helloCont.join(" ");
+class HelloWorld extends React.Component {
+  render() {
+    const cont = ["Africa", "America", "AsiaAAAA"];
+    const helloCont = Array.from(cont, c => `Hello ${c}`);
+    const message = helloCont.join(" ");
 
-const element = (
-  <div title="Outer div">
-    <h1>{message}</h1>
-  </div>
-);
+    return (
+      <div title="Outer div">
+        <h1>{message}</h1>
+      </div>
+    );
+  }
+}
+const element = <HelloWorld />;
 
 // render
-ReactDOM.render(element, document.getElementById("content"));
+ReactDOM.render(element, document.getElementById("contents"));
